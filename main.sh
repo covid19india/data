b7183e61203ede09a1a5338b7640dbb8e12e08d2
PraveenKPandu <PraveenKPandu@bots.github.com>
2022-01-16 00:12:44 +0000

    A bit of housekeeping & code dir houses files

--- main.sh
+++ main.sh
@@ -32,56 +32,64 @@ git config user.email "${GITHUB_ACTOR}@bots.github.com"
 # Download all necessary files from repo branches in to code directory
 if [ -d "${CODE_DIR}" ]; then
   echo "${CODE_DIR} directory exists"
+  cd ${CODE_DIR}
 else
   echo "Creating new directory named ${CODE_DIR}..."
   mkdir -p ${CODE_DIR} && cd $_
 fi
 
-git clone -b ${GH_PAGES_BRANCH} $repo_uri
-
-# # Checkout gh pages branch
-# git checkout "${GH_PAGES_BRANCH}"
+# Chekout repo branches in respective folders
+git clone --depth 1 -b ${GH_PAGES_BRANCH} $repo_uri ${GH_PAGES_BRANCH}
+git clone --depth 1 -b ${MAIN_BRANCH} $repo_uri ${MAIN_BRANCH}
 
 if [ -d "${TEMP_DIR}" ]; then
-  echo "tmp directory exists"
+  echo "${TEMP_DIR} directory exists"
 else
   echo "Creating new directory named ${TEMP_DIR}..."
   mkdir ${TEMP_DIR}
 fi
 
-# Copying files to respective folders
-cp -r ./updatelog ./tmp
-cp -r ./csv ./tmp
-cp v4/min/data.min.json ./tmp/data-old.min.json
-cp ./csv/latest/state_wise.csv ./tmp/state_wise_prev
+# Verify the existence of the directory
+if [ -d "${GH_PAGES_BRANCH}" ]; then
+  echo "${GH_PAGES_BRANCH} directory exists"
+  # Copying files to respective folders
+  cp -r ${GH_PAGES_BRANCH}/updatelog ${TEMP_DIR}
+  cp -r ${GH_PAGES_BRANCH}/csv ${TEMP_DIR}
+  cp ${GH_PAGES_BRANCH}/v4/min/data.min.json ${TEMP_DIR}/data-old.min.json
+  cp ${GH_PAGES_BRANCH}/csv/latest/state_wise.csv ${TEMP_DIR}/state_wise_prev
+else
+  echo "${GH_PAGES_BRANCH} not found. Exiting..."
+fi
 
-# Checkout Main branch
-git checkout "${MAIN_BRANCH}"
-
-# Copying the documentation files to temporary folder
-cp README.md tmp/
-cp -r documentation/ tmp/
+# Verify the existence of the directory
+if [ -d "${MAIN_BRANCH}" ]; then
+  echo "${MAIN_BRANCH} directory exists"
+  # Copying files to respective folders
+  cp ./${MAIN_BRANCH}/README.md ${TEMP_DIR}/
+  cp -r ./${MAIN_BRANCH}/documentation/ ${TEMP_DIR}/
+else
+  echo "${MAIN_BRANCH} not found. Exiting..."
+fi
 
 # Convert the google sheet data to csv using Node.js script
-node src/sheets-to-csv.js
+node ./${MAIN_BRANCH}/src/sheets-to-csv.js
 
 # Invoke the Python Parser 4 script to generate the json data for api calls
-python3 src/parser_v4.py
-python3 src/generate_activity_log.py
+python3 ./${MAIN_BRANCH}/src/parser_v4.py
+python3 ./${MAIN_BRANCH}/src/generate_activity_log.py
 # node src/sanity_check.js # need rewrite with new json
 
-# Switch to GH Pages Branch again
-git checkout "${GH_PAGES_BRANCH}"
+# # Remove the old files from tmp directory
+rm ${TEMP_DIR}/data-old.min.json
+rm ${TEMP_DIR}/state_wise_prev
 
-# Remove the old files from tmp directory
-rm tmp/data-old.min.json
-rm tmp/state_wise_prev
+# # Copy everything from tmp directory to root folder and remove tmp directory
+cp -r ${TEMP_DIR}/* ${GH_PAGES_BRANCH}
+rm -r ${TEMP_DIR}/
 
-# Copy everything from tmp directory to root folder and remove tmp directory
-cp -r tmp/* .
-rm -r tmp/
+cd ${GH_PAGES_BRANCH}
 
-# Add all the files to the repo and commit
+# # Add all the files to the repo and commit
 git add .
 set +e  # Grep succeeds with nonzero exit codes to show results.
 
@@ -96,5 +104,7 @@ else
     set -e
     echo "No changes since last run"
 fi
+
+rm -rf ../../${CODE_DIR}/
 
 echo "main.sh end"
